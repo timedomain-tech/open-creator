cd2f7cfd30dcc599415642db3fbdd7639739d4a2
Gong Junmin <1836678486@qq.com>
2023-10-07 10:11:40 +0800

    feat: add contributing doc for Japenese and Chineses

--- tests/test_creator_agent.py
+++ tests/test_creator_agent.py
@@ -14,8 +14,6 @@ def test_config_path():
 def test_run_create_skill():
     create_skill = creator.create(skill_path=creator.config.build_in_skill_config["create"])
     skill = create_skill.run("create a skill that request is 'open the chrome and go to www.google.com in my mac'")
-    # save_skill = creator.create(skill_path=creator.config.build_in_skill_config["save"])
-    # creator.save(skill)
     print(repr(skill))
 
 if __name__ == "__main__":
