f0b3d5f1c9a7ce5a2f6331e588ee2771389f6979
Gong Junmin <1836678486@qq.com>
2023-10-31 18:42:18 +0800

    fix: fix python shell code startswith '!'

--- creator/code_interpreter/__init__.py
+++ creator/code_interpreter/__init__.py
@@ -66,6 +66,13 @@ class CodeInterpreter(StructuredTool):
         # replace tab to 4
         return code
 
+    def hot_fix(self, language, code):
+        # fix python code start with !
+        if language == "python" and code.startswith("!"):
+            language = "shell"
+            code = code[1:]
+        return language, code
+
     def _run(
         self,
         language: str,
@@ -76,6 +83,7 @@ class CodeInterpreter(StructuredTool):
         language = language.lower()
         if language not in language_map:
             return {"status": "error", "stdout": "", "stderr": f"Language {language} not supported, Only support {list(language_map.keys())}"}
+        language, code = self.hot_fix(language, code)
         if language not in self.interpreters:
             self.add_interpreter(language=language)
         code = self.clean_code(code)
