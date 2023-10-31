2ab4fcdb31d2fabe5ed2ebc707a82a7644d251ad
Gong Junmin <1836678486@qq.com>
2023-10-31 18:20:16 +0800

    fix: check langsmith ok

--- creator/utils/langsmith_utils.py
+++ creator/utils/langsmith_utils.py
@@ -1,21 +1,22 @@
+import os
 from langsmith import Client
-from langsmith.utils import LangSmithError
-from .printer import print
+from langsmith.utils import LangSmithConnectionError
+from printer import print
 
 
 def check_langsmith_ok():
     cli = Client()
-    ok = False
+    if os.environ.get("LANGCHAIN_TRACING_V2", "false") == "false":
+        return False
     try:
         cli.read_project(project_name="open-creator")
-    except LangSmithError as e:
-        if "Bad Gateway" in str(e):
+    except LangSmithConnectionError as e:
+        if "Connection error" in str(e):
             print("[red]Warning:[/red] [yellow]Langsmith is not running. Please run `langsmith start`.[/yellow]")
-            return ok
+            return False
         else:
             cli.create_project(project_name="open-creator")
-    ok = True
-    return ok
+    return True
 
 
 langsmith_ok = check_langsmith_ok()
